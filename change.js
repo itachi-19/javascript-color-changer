044dfb09a0e74032d10b2b0d8b052223a9360d83
itachi-19 <sulabh.biswas.0157@gmail.com>
2021-02-25 21:11:40 +0530

    bold heading, change of text color upon clicking Change button

--- change.js
+++ change.js
@@ -9,18 +9,28 @@
 let hexes = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];  
 let body_el = document.querySelector('body');
 let current_color_el = document.querySelector('b');
-let white = '#FFFFFF';
+let title = document.getElementById('title');
+let instruction = document.getElementById('instruction');
+let running = false;
 
 function getRandomIdx() {
     return Math.floor(Math.random()*16);
 }
 
 function reset() {
-    body_el.style.backgroundColor = white;
-    current_color_el.innerText = white;
+    running = false;
+    title.style.color = 'black';
+    instruction.style.color = 'black';
+    body_el.style.backgroundColor = 'white';
+    current_color_el.innerText = '#FFFFFF';
 }
 
 function change() {
+    if (!running) {
+        title.style.color = 'white';
+        instruction.style.color = 'white';
+        running = true;
+    }
     let new_color = '#';
     for (let i=0;i<6;i++) {
         new_color += hexes[getRandomIdx()];
